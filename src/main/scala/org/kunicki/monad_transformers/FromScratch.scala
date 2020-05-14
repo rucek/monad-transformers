7753289593235035db885245661f7f5bed39ff8a
Jacek Kunicki <jacek@kunicki.org>
2020-05-14 16:36:36 +0200

    Using OptionInsideFuture

--- src/main/scala/org/kunicki/monad_transformers/FromScratch.scala
+++ src/main/scala/org/kunicki/monad_transformers/FromScratch.scala
@@ -8,15 +8,25 @@ case class Company(id: Long, name: String)
 
 class FromScratch {
 
-  type Effect[A] = Future[Option[A]]
+  type Effect[A] = OptionInsideFuture[A]
 
   private def findUserById(id: Long): Effect[User] = ???
 
   private def findCompanyByUser(user: User): Effect[Company] = ???
 
   def findCompanyByUserId(id: Long)(implicit ec: ExecutionContext): Future[Option[Company]] =
-    for {
+    (for {
       user <- findUserById(id)
       company <- findCompanyByUser(user)
-    } yield company
+    } yield company).value
+}
+
+case class OptionInsideFuture[A](value: Future[Option[A]])(implicit ec: ExecutionContext) {
+
+  def map[B](f: A => B): OptionInsideFuture[B] = OptionInsideFuture(value.map(_.map(f)))
+
+  def flatMap[B](f: A => OptionInsideFuture[B]): OptionInsideFuture[B] = OptionInsideFuture(value.flatMap {
+    case Some(a) => f(a).value
+    case None => Future.successful(None)
+  })
 }
